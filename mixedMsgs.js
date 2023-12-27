207bbc2097c725962c3f5a44905e09f0a038f00f
Matt <matthewpaine0106@gmail.com>
2023-12-27 09:08:01 +0000

    First Commit

---
+++ mixedMsgs.js
@@ -0,0 +1,11 @@
+const begin = [];
+const person = [];
+const name = [];
+const attrib1 = [];
+const attrib2 = [];
+
+let baseStory = [];
+
+// Function to loop and insert one of each eleemnt randomly into story text
+
+// Function to display story text when btn clicked
